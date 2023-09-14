ace77864a6cd0caa822f9756f8865ea82c3df32f
jaewoong <jwsuh25@kis.ac>
2023-09-14 10:10:26 +0900

    Initial version

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
+import io
 import json
 import os
 import time
+import zipfile
 
-from flask import Flask, send_from_directory, request, redirect, url_for, session
-from werkzeug.utils import secure_filename
+from flask import Flask, send_from_directory, request, redirect, url_for, session, send_file
 
 from auth import Authenticator
 
 # ==================== Initialize app and login manager ====================
-config = json.load(open('config.json'))
+with open('config.json') as f:
+    config = json.load(f)
 app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 app.secret_key = config['secret_key']
 
 authenticator = Authenticator()
-MAX_TRIALS = 3
-LOCKOUT_DURATION = 60
+LOCKOUT_THRESHOLDS = config['security']['lockout-thresholds']
+
+
+# ==================== Lockdown functions ====================
+def get_lockout_duration(failed_attempts):
+    """Return lockout duration based on the number of failed attempts."""
+    for threshold, duration in sorted(LOCKOUT_THRESHOLDS.items(), key=lambda x: int(x[0])):
+        if failed_attempts >= int(threshold):
+            return duration
+    return 0
 
 
 # ==================== Login manager callback functions ====================
-
-
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if 'username' in session:
@@ -27,24 +35,25 @@ def login():
     return send_from_directory('static', 'html/login.html')
 
 
-@app.route('/home/')
-def home():
-    if 'username' in session:
-        return send_from_directory('static', 'html/home.html')
-    return redirect(url_for('login'))  # Redirect to login page if the user is not logged in
-
-
 @app.route('/auth/<path:action>', methods=['POST', 'GET'])
 def auth_route(action):
-    if action in ['authenticate', 'authenticate/']:
+    if not config['security']['enabled']:
+        # If security is not enabled, let's allow authentication always
+        return {"success": True, "message": "Security is disabled."}
 
-        # Check if user is locked out
+    if action in ['authenticate', 'authenticate/']:
         if session.get('lockout_time'):
             time_elapsed = time.time() - session['lockout_time']
-            if time_elapsed < LOCKOUT_DURATION:
+            lockout_duration = get_lockout_duration(session.get('failed_attempts', 0))
+
+            if lockout_duration == -1:  # Permanent lockout
+                return {"success": False,
+                        "message": "Your account is locked permanently due to too many failed attempts."}
+
+            if time_elapsed < lockout_duration:
+                print(lockout_duration)
                 return {"success": False, "message": "Too many failed attempts. Please try again later."}
             else:
-                # Reset after lockout duration
                 session.pop('lockout_time', None)
                 session.pop('failed_attempts', None)
 
@@ -53,15 +62,17 @@ def auth_route(action):
         time.sleep(1)
 
         if authenticator.authenticate(username, password):
-            session['username'] = username  # Storing the username in session
-            # Reset the failed attempts on successful login
+            session['username'] = username
             session.pop('failed_attempts', None)
             return {"success": True, "message": f"Welcome {session['username']}!"}
         else:
-            # Increment the failed attempts counter
             session['failed_attempts'] = session.get('failed_attempts', 0) + 1
-            if session['failed_attempts'] >= MAX_TRIALS:
+            lockout_duration = get_lockout_duration(session['failed_attempts'])
+            if lockout_duration > 0:
                 session['lockout_time'] = time.time()
+                if lockout_duration == -1:  # Permanent lockout
+                    return {"success": False,
+                            "message": "Your account is locked permanently due to too many failed attempts."}
                 return {"success": False, "message": "Too many failed attempts. Please try again later."}
             else:
                 return {"success": False, "message": "Invalid username or password!"}
@@ -75,6 +86,13 @@ def auth_route(action):
 
     else:
         return {"success": False, "message": "Invalid action!"}
+
+
+@app.route('/home/')
+def home():
+    if 'username' in session:
+        return send_from_directory('static', 'html/home.html')
+    return redirect(url_for('login'))  # Redirect to login page if the user is not logged in
 
 
 @app.route('/api/<path:action>', methods=['GET', 'POST'])
@@ -99,6 +117,56 @@ def api_route(action):
         else:
             return {"exists": False}
 
+    elif action == 'getpages/' or action == 'getpages':
+        # check the folder for the user
+        username = session['username']
+        user_dir = os.path.join('userdata', username)
+        if not os.path.exists(user_dir):
+            return {"pages": []}
+        # get all the folders in the user's folder
+        pages = os.listdir(user_dir)
+        # remove dot files
+        pages = [page for page in pages if not page.startswith('.')]
+        return {"pages": pages}
+
+    elif action == 'download/' or action == 'download':
+        # check the folder for the user
+        username = session['username']
+        slug = request.form.get('slug', '')
+        user_dir = os.path.join('userdata', username)
+        slug_dir = os.path.join(user_dir, slug)
+        if not os.path.exists(slug_dir):
+            return {"success": False, "message": "Slug does not exist!"}
+
+        # get all the files in the user's folder
+        files = os.listdir(slug_dir)
+        files = [file for file in files if not file.startswith('.')]
+        # save the zip file in memory
+        zip_buffer = io.BytesIO()
+        with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for file in files:
+                file_path = os.path.join(slug_dir, file)
+                zipf.write(file_path, file)
+
+        # Move buffer position to the beginning after writing
+        zip_buffer.seek(0)
+        # Return the zip file
+        return send_file(zip_buffer, download_name=f"{slug}-webfile.zip", as_attachment=True,
+                         mimetype='application/zip')
+
+    elif action == 'delete/' or action == 'delete':
+        username = session['username']
+        slug = request.form.get('slug', '')
+        user_dir = os.path.join('userdata', username)
+        slug_dir = os.path.join(user_dir, slug)
+        if not os.path.exists(slug_dir):
+            return {"success": False, "message": "Slug does not exist!"}
+        # delete the folder
+        import shutil
+        shutil.rmtree(slug_dir)
+        return {"success": True, "message": "Slug deleted successfully!"}
+
+
     elif action == 'upload/' or action == 'upload':
         username = session['username']
         slug = request.form.get('slug', '')
@@ -109,7 +177,7 @@ def api_route(action):
             return {"success": False, "message": "Slug already exists!"}
         if slug == '':  # check if slug is empty
             return {"success": False, "message": "Slug cannot be empty!"}
-        if not slug.isalnum():  # check if slug is alphanumeric
+        if not slug.replace("-", "").isalnum(): # check if slug is alphanumeric
             return {"success": False, "message": "Slug must be alphanumeric!"}
         if len(slug) > 20:  # check if slug is less than 20 characters
             return {"success": False, "message": "Slug must be less than 20 characters!"}
@@ -130,22 +198,43 @@ def api_route(action):
 
         # Save each file
         for file in files:
-            filename = secure_filename(file.filename)
-            print(filename)
-            file_path = os.path.join(slug_dir, filename)
-            file.save(file_path)
-        return {"success": True, "message": "Files uploaded successfully!"}
+            if file.filename.startswith('.'):  # Skip dot files
+                continue
 
+            # Split the path and discard the first component, which is the folder name
+            parts = file.filename.split(os.path.sep)[1:]
+
+            # Join the parts again to get the new relative path without the folder name
+            new_relative_path = os.path.join(*parts)
+
+            file_path = os.path.join(slug_dir, new_relative_path)
+
+            # Create necessary directories for the current file
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            file.save(file_path)
+
+        return {"success": True, "message": "Files uploaded successfully!"}
     else:
         return {"success": False, "message": "Invalid action!"}
 
 
-@app.route('/sites/<path:path>')
-def serve_site(path):
-    print(path)  # jay/hello/ -> jay/hello/index.html
-    if path.endswith('/'):
-        path += 'index.html'
-    return send_from_directory('userdata', path)
+@app.route('/sites/<user>/<slug>/', defaults={'path': ''})
+@app.route('/sites/<user>/<slug>/<path:path>')
+def serve_site(user, slug, path=''):
+    if not path:
+        path = 'index.html'
+        # check if file exists
+        if not os.path.exists(os.path.join('userdata', user, slug, path)):
+            # try to serve first html file
+            for file in os.listdir(os.path.join('userdata', user, slug)):
+                if file.endswith('.html'):
+                    path = file
+                    break
+
+    if os.path.exists(os.path.join('userdata', user, slug, path)):
+        return send_from_directory(os.path.join('userdata', user, slug), path)
+    else:
+        return send_from_directory('static', 'html/404.html')
 
 
 if __name__ == '__main__':
--- static/js/home.js
+++ static/js/home.js
@@ -4,11 +4,20 @@ const app = Vue.createApp({
             isSubmitting: false,
             isUploading: false,
             userName: '',
+            domain: window.location.origin,
             filesValid: false,
             slug: '',
             slugWarning: null,
             slugMessage: '',
-            slugValid: false
+            slugValid: false,
+
+            isDialogOpen: false,
+            dialogTitle: '',
+            dialogMessage: '',
+
+            sites: [],
+            isDeleting: false,
+
         };
     },
     computed: {
@@ -18,6 +27,56 @@ const app = Vue.createApp({
         },
     },
     methods: {
+        async deleteSite(slug) {
+            try {
+                this.isDeleting = true;  // Set this flag to true to indicate the delete operation is in progress
+
+                let response = await fetch('/api/delete', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: `slug=${encodeURIComponent(slug)}`
+                });
+
+                let data = await response.json();
+
+                if (data.success) {
+                    await this.fetchSites();  // Reload the list of sites
+                    this.openDialog('👍 Success!', 'Your website, <del>' + slug + '</del> was deleted successfully! 💥')
+                } else {
+                    this.openDialog('😓 Error!', 'There was an error deleting your website. Please try again. ??')
+                }
+            } catch (error) {
+                console.error('There was an error deleting the slug:', error);
+                this.openDialog('😓 Error!', 'There was an error deleting your website. Please try again.')
+            } finally {
+                this.isDeleting = false;  // Reset the flag
+            }
+        },
+        downloadSite(slug) {
+            // Create an invisible form and submit it to initiate the download
+            let form = document.createElement('form');
+            form.method = 'POST';
+            form.action = '/api/download';
+            form.style.display = 'none';
+
+            let input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = 'slug';
+            input.value = slug;
+
+            form.appendChild(input);
+            document.body.appendChild(form);
+
+            form.submit();
+
+            document.body.removeChild(form);
+
+            // reload the list of sites
+            this.fetchSites();
+        },
+
         async logout() {
             this.isSubmitting = true;
             try {
@@ -33,11 +92,12 @@ const app = Vue.createApp({
                 if (data.success) {
                     window.location.href = '/';
                 } else {
-                    alert('Logout failed. Please try again.');
+                    console.error(data.message);
+                    this.openDialog('😓 Error!', 'There was an error logging you out. Please try again.')
                 }
             } catch (error) {
                 console.error('There was an error:', error);
-                alert('An error occurred. Please try again.');
+                this.openDialog('😓 Error!', 'There was an error logging you out. Please try again.')
             } finally {
                 this.isSubmitting = false;
             }
@@ -76,14 +136,31 @@ const app = Vue.createApp({
 
             this.filesValid = hasIndexHtml;
         },
+
+        async fetchSites() {
+            this.sites = [];
+            try {
+                const response = await fetch('/api/getpages/', {
+                    method: 'GET',
+                    headers: {'Content-Type': 'application/json'}
+                });
+                const data = await response.json();
+                if (data.pages) {
+                    this.sites = data.pages.map(slug => ({slug, url: `${this.domain}/sites/${this.userName}/${slug}`}));
+                } else {
+                    console.error('Error fetching pages:', data.message);
+                }
+            } catch (error) {
+                console.error('Error fetching user pages:', error);
+            }
+        },
+
         async checkSlug() {
             if (!this.slug) {
                 this.slugWarning = null;
                 return;
             }
             try {
-
-
                 if (!/^[a-zA-Z0-9-]+$/.test(this.slug)) {
                     this.slugWarning = true;
                     this.slugValid = false;
@@ -116,13 +193,20 @@ const app = Vue.createApp({
             const folderInput = this.$refs.folderInput;
 
             if (!folderInput.files.length) {
-                alert('Please select a folder to upload.');
+                this.openDialog('💻 Error!', 'Please select a folder to upload.')
                 return;
             }
 
             const formData = new FormData();
             for (let i = 0; i < folderInput.files.length; i++) {
-                formData.append('file', folderInput.files[i]);
+                // Skip dot files
+                if (folderInput.files[i].name.startsWith('.')) {
+                    continue;
+                }
+
+                // Use webkitRelativePath to preserve the directory structure.
+                const path = folderInput.files[i].webkitRelativePath || folderInput.files[i].name;
+                formData.append('file', folderInput.files[i], path);
             }
             formData.append('slug', this.slug);
 
@@ -137,16 +221,30 @@ const app = Vue.createApp({
                 const data = await response.json();
 
                 if (data.success) {
-                    alert('Folder uploaded successfully!');
+                    await this.fetchSites();
+                    this.url = this.domain + '/sites/' + this.userName + '/' + this.slug;
+                    this.openDialog('👍 Success!', 'Your website was uploaded successfully! 🥳<br> <a href="' + this.url + '" target="_blank">' + this.url + '</a>')
+                    this.$refs.folderInput.value = '';
+                    this.slug = '';
+                    this.filesValid = false;
+                    this.slugWarning = null;
+                    this.slugMessage = '';
+                    this.slugValid = false;
+
                 } else {
-                    alert(data.message || 'Upload failed. Please try again.');
+                    this.openDialog('🔥Error!', data.message || 'Upload failed. Please try again.')
                 }
             } catch (error) {
                 console.error('There was an error uploading the folder:', error);
-                alert('An error occurred. Please try again.');
+                this.openDialog('😓 Error!', 'There was an error uploading your website. Please try again.')
             } finally {
                 this.isUploading = false;
             }
+        },
+        async openDialog(title, message) {
+            this.dialogTitle = title;
+            this.dialogMessage = message;
+            this.isDialogOpen = true;
         }
     },
     watch: {
@@ -154,6 +252,7 @@ const app = Vue.createApp({
     },
     mounted() {
         this.fetchUserName();
+        this.fetchSites();
     }
 });
 
